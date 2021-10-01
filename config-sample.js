84dfa6f215046ee90588e7209ca34460c673d8aa
Samuel Rinnetmäki <samuel@iki.fi>
2021-10-01 07:53:26 +0300

    changed tresholds to kWh prices instead of MWh, added debugLevel

--- config-sample.js
+++ config-sample.js
@@ -1,12 +1,14 @@
 const config = {
   area: 'FI', // see http://www.nordpoolspot.com/maps/
   currency: 'EUR', // can also be 'DKK', 'NOK', 'SEK'
-  highTreshold: 60, // send event when price > highTreshold EUR/MWh
-  lowTreshold: 30, // send event when price < lowTreshold EUR/MWh
+  currencySubUnit: 'snt', // 1/100 of currency, used to format the message passed to IFTTT, can be 'cents', 'öre', 'øre', ...
+  highTreshold: 8, // send event when price > highTreshold
+  lowTreshold: 5, // send event when price < lowTreshold
   maxHighHours: 24, // max consecutive high hours
   maxLowHours: 24, // max consecutive low hours
   vatPercent: 24, // if you want prices including value-added tax (VAT), enter the percentage here
-  iftttKey: 'CHANGE' // see https://ifttt.com/services/maker_webhooks/settings
+  iftttKey: 'CHANGE', // see https://ifttt.com/services/maker_webhooks/settings
+  debugLevel: 1
 };
 
 export {config};
